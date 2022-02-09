cfb6afaa1ff92890d5e8eecae66a0662b5f71380
MARTIN.DOSSANTOS <martin.dossantos@naranja.com>
2022-02-08 21:23:40 -0300

    fix puerto

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
+require("dotenv").config();
 const express = require('express');
 const app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor funcionando');
 });
 
